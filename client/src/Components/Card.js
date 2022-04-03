7bd288908ba9f31688664b93fd741c8e7297ac88
BrandonCHW <brad_1800@hotmail.com>
2022-04-02 22:02:24 -0400

    create card. add boostrap

---
+++ client/src/Components/Card.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
+
+export class Card extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                <Button>
+                    hello
+                </Button>
+            </div>
+        )
+    }
+}
