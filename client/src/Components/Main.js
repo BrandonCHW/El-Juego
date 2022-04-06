572900d4a50e293ac427d2e1db3a6934d3323e4b
BrandonCHW <brad_1800@hotmail.com>
2022-04-05 21:35:31 -0400

    move connect button to main

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './Main.css';
 import PlayerHand from './PlayerHand'
 import CenterBoard from './CenterBoard';
+import { Button } from 'react-bootstrap';
+import io from 'socket.io-client'
 
 function Main(props) {
     // 1 state per values that tend to change together
@@ -11,10 +13,27 @@ function Main(props) {
     const [playerOneCards, setPlayerOneCards] = useState(['1','2','3','97','98','99'])
     const [playerTwoCards, setPlayerTwoCards] = useState(['1','2','3','97','98','99'])
 
-    useEffect(() => {
-        console.log('lobby is loaded')
-    })
+    
+    const [connect, setConnect] = useState(false)
+    const [socket, setSocket] = useState(null)
         
+    useEffect(() => {
+        if (connect) {
+        const newSocket = io(`http://${window.location.hostname}:4000`) 
+        console.log(`connected to: http://${window.location.hostname}:4000`)
+        setSocket(newSocket)
+        
+        return () => {
+            newSocket.close() // si la page rafraichie, va etablir une nouvelle connexion...
+        }
+        }
+    }, [connect, setSocket])
+
+
+    const connectToServer = () => {
+        setConnect(!connect)
+      }
+    
     const handlePlayCard = (playerId, playedCardValue) => {
         if (playerId === 1) {
             setSelectedPlayedOneCard(playedCardValue)
@@ -23,6 +42,8 @@ function Main(props) {
             setSelectedPlayedTwoCard(playedCardValue)
         }
         console.log(`player ${playerId} played card ${playedCardValue} on pile ${selectedPileId}`)
+
+        // emit here
     }
 
     return ( 
@@ -35,6 +56,15 @@ function Main(props) {
             }/>
             <div>
                 <PlayerHand name="Player1" hand={playerOneCards} onPlay={(value) => handlePlayCard(1, value)}/>
+                <Button onClick={() => connectToServer()}>P1 connect</Button>
+                { connect ? 
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16" color='lightgreen'>
+                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                </svg>
+                :       
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16" color='red'>
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg> }
                 <PlayerHand name="Player2" hand={playerTwoCards} onPlay={(value) => handlePlayCard(2, value)}/>
             </div>
         </div> 
--- client/src/Components/PlayerHand.js
+++ client/src/Components/PlayerHand.js
@@ -4,22 +4,7 @@ import { Button } from "react-bootstrap";
 import io from 'socket.io-client'
 
 function PlayerHand(props) {
-  const [connect, setConnect] = useState(false)
-  const [socket, setSocket] = useState(null)
   const [selectedCard, setSelectedCard] = useState({ key: -1, value: 0 });
-
-  useEffect(() => {
-    if (connect) {
-      console.log('BITCONNECT BECAUSE connect IS: ', connect)
-      const newSocket = io(`http://${window.location.hostname}:4000`) 
-      setSocket(newSocket)
-      // console.log('new socket connection:', socket)
-  
-      return () => {
-        newSocket.close() // si la page rafraichie, va etablir une nouvelle connexion...
-      }
-    }
-  }, [connect, setSocket])
 
   const handleClick = (event, card) => {
     setSelectedCard(card);
@@ -33,11 +18,6 @@ function PlayerHand(props) {
       props.onPlay(selectedCard.value);
     }
   };
-
-  const connectToServer = () => {
-    setConnect(true)
-  }
-
   return (
     // todo make them radio buttons
     <div>
@@ -55,15 +35,6 @@ function PlayerHand(props) {
         );
       })}{" "}
       <Button onClick={() => handlePlayCard()}>Play Card</Button>{' '}
-      <Button onClick={() => connectToServer()}>Bitconnect</Button>
-      { connect ? 
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16" color='lightgreen'>
-        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
-      </svg>
-      :       
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16" color='red'>
-        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-      </svg> }
     </div>
   );
 }
