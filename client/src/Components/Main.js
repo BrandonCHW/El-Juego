3bddc79352e4241eb604d107a4ab801ec4802359
BrandonCHW <brad_1800@hotmail.com>
2022-04-06 21:09:17 -0400

    get hands from gamestate

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -60,10 +60,12 @@ function Main(props) {
         }
     }
 
+    /******* DEV-TOOLS ONLY ********/ 
     const HandleManualNewGameState = (state) => {
         console.log(state)
         setGameState(state)
     }
+    /*******************************/ 
 
     return ( 
         <>
@@ -77,7 +79,7 @@ function Main(props) {
                         setSelectedPileId(id)
                     }}/>
                 <div>
-                    <PlayerHand name="Player1" hand={playerOneCards} onPlay={(value) => handlePlayCard(1, value)}/>
+                    <PlayerHand name="Player1" hand={gameState.hands[0] ?? playerOneCards} onPlay={(value) => handlePlayCard(1, value)}/>
                     <Button onClick={() => connectToServer()}>P1 connect</Button>
                     { connect ? 
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16" color='lightgreen'>
@@ -87,7 +89,7 @@ function Main(props) {
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16" color='red'>
                         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                     </svg> }
-                    <PlayerHand name="Player2" hand={playerTwoCards} onPlay={(value) => handlePlayCard(2, value)}/>
+                    <PlayerHand name="Player2" hand={gameState.hands[1] ?? playerTwoCards} onPlay={(value) => handlePlayCard(2, value)}/>
                 </div>
             </div> 
         </>
