ad9095831600fb31205d5ee9b60b07bab426b968
BrandonCHW <brad_1800@hotmail.com>
2022-04-16 01:43:06 -0400

    nothing

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -170,39 +170,6 @@ function Main(props) {
             </svg>
           )}
           <Button onClick={() => handleEndTurn(socket, uid1.current)} disabled={gameState.turn!==uid1.current} variant='secondary' size='sm'>P1 End Turn</Button>
-
-          <PlayerHand
-            name={"Player2 (" + uid2.current + ")"}
-            hand={gameState?.hands.find(h => h.uid === uid2.current)?.cards ?? [0, 0, 0, 0, 0, 0]}
-            onPlay={(value) => handlePlayCard(socket2, uid2, value)}
-          />
-          <Button onClick={() => connectToServer2()}>P2 connect</Button>
-          {connect2 ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="currentColor"
-              className="bi bi-check"
-              viewBox="0 0 16 16"
-              color="lightgreen"
-            >
-              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="currentColor"
-              className="bi bi-x"
-              viewBox="0 0 16 16"
-              color="red"
-            >
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-            </svg>
-          )}
-          <Button onClick={() => handleEndTurn(socket2, uid2.current)} disabled={gameState.turn!==uid2.current} variant='secondary' size='sm'>P2 End Turn</Button>
         </div>
       </div>
     </>
